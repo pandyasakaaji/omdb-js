bf70c3dd036dc393da7d5f65397cc66b28bba790
Saka <pandyasakaaji@gmail.com>
2026-07-31 01:03:01 +0700

    refactor: create function for cards and modal template
    fix: add return to cards and modal template function

--- js/script.js
+++ js/script.js
@@ -11,16 +11,7 @@ fetch(getAllMovies('ben'))
     let cards = ''
 
     movies.forEach(movie => {
-      cards += ` <div class="col-md-4 my-3">
-        <div class="card">
-          <img src="${movie.Poster}" class="card-img-top" alt="">
-          <div class="card-body d-flex flex-column justify-content-center align-items-center text-center">
-            <h5 class="card-title">${movie.Title}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">${movie.Year}</h6>
-            <a class="btn btn-primary movie-details-button" data-bs-toggle="modal" data-bs-target="#movieModal" data-imdbid="${movie.imdbID}">Show Details</a>
-          </div>
-        </div>
-      </div> `
+      cards += showCards(movie)
     })
 
     row.innerHTML = cards
@@ -33,27 +24,43 @@ row.addEventListener('click', event => {
     fetch(getSingleMovie(query))
       .then(res => res.json())
       .then(res => {
-        let modalBody = `<div class="container-fluid">
-              <div class="row">
-                <div class="col-md-3">
-                  <img src="${res.Poster}" alt="" class="img-fluid">
-                </div>
-                <div class="col-md">
-                  <ul class="list-group">
-                    <li class="list-group-item">
-                      <h4>${res.Title}</h4>
-                    </li>
-                    <li class="list-group-item"><strong>Director: ${res.Director}</strong></li>
-                    <li class="list-group-item"><strong>Actor: ${res.Actors}</strong></li>
-                    <li class="list-group-item"><strong>Writer: ${res.Writer}</strong></li>
-                    <li class="list-group-item"><strong>Plot: <br></strong> ${res.Plot}</li>
-                  </ul>
-                </div>
-              </div>
-            </div>`
-
+        let modalBody = showModal(res)
         modal.innerHTML = modalBody
       })
       .catch(err => console.log(err))
   }
 })
+
+function showCards(movie) {
+  return `<div class="col-md-4 my-3">
+      <div class="card">
+        <img src="${movie.Poster}" class="card-img-top" alt="">
+        <div class="card-body d-flex flex-column justify-content-center align-items-center text-center">
+          <h5 class="card-title">${movie.Title}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">${movie.Year}</h6>
+          <a class="btn btn-primary movie-details-button" data-bs-toggle="modal" data-bs-target="#movieModal" data-imdbid="${movie.imdbID}">Show Details</a>
+        </div>
+      </div>
+    </div> `
+}
+
+function showModal(res) {
+  return `<div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="${res.Poster}" alt="" class="img-fluid">
+      </div>
+      <div class="col-md">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <h4>${res.Title}</h4>
+          </li>
+          <li class="list-group-item"><strong>Director: ${res.Director}</strong></li>
+          <li class="list-group-item"><strong>Actor: ${res.Actors}</strong></li>
+          <li class="list-group-item"><strong>Writer: ${res.Writer}</strong></li>
+          <li class="list-group-item"><strong>Plot: <br></strong> ${res.Plot}</li>
+        </ul>
+      </div>
+    </div>
+  </div>`
+}
